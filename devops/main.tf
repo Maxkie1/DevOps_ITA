57ed547e54388f6ff1a00a49a4715c0b0bacff6d
Max Kiefer <inf20111@lehre.dhwb-stuttgart.de>
2022-11-22 12:54:00 +0100

    add terraform variables

--- devops/main.tf
+++ devops/main.tf
@@ -12,7 +12,7 @@ provider "azurerm" {
   features {}
 }
 
-# Generate a random integer to create  a globally unique name
+# Generate a random integer to create a globally unique name
 resource "random_integer" "ri" {
   min = 1000
   max = 9999
@@ -20,29 +20,34 @@ resource "random_integer" "ri" {
   
 # Create a resource group
 resource "azurerm_resource_group" "rg" {
-  name = "myResourceGroup-${random_integer.ri.result}"
-  location = "eastus2"
+  name = "${var.rg_name}-${random_integer.ri.result}"
+  location = var.rg_location
 }
 
 # Create the app service plan
 resource "azurerm_service_plan" "appserviceplan" {
-    name = "myAppServicePlan-${random_integer.ri.result}"
+    name = "${var.app_service_plan_name}-${random_integer.ri.result}"
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
     sku_name =  "F1"
     os_type = "Linux"
 }
 
+# Create the app service
 resource "azurerm_linux_web_app" "app" {
-    name = "myApp-${random_integer.ri.result}"
+    name = "${var.app_name}-${random_integer.ri.result}"
     location = azurerm_resource_group.rg.location
     resource_group_name = azurerm_resource_group.rg.name
     service_plan_id = azurerm_service_plan.appserviceplan.id
     site_config {
         always_on = false
         application_stack {
-            docker_image = "maxkie/devops-devops"
-            docker_image_tag = "latest"
+            docker_image = var.app_image
+            docker_image_tag = var.app_image_tag
         }
     }
+    app_settings = {
+        "WEBSITES_PORT" = var.app_port
+    }
+
 }
---
+++ devops/variables.tf
@@ -0,0 +1,34 @@
+variable "rg_name" {
+    description = "The name of the resource group"
+    default = "myResourceGroup"
+}
+
+variable "rg_location" {
+    description = "The location of the resource group"
+    default = "eastus2"
+}
+
+variable "app_service_plan_name" {
+    description = "The name of the app service plan"
+    default = "myAppServicePlan"
+}
+
+variable "app_name" {
+    description = "The name of the app service"
+    default = "myApp"
+}
+
+variable "app_image" {
+    description = "The name of the app image"
+    default = "maxkie/devops-devops"
+}
+
+variable "app_image_tag" {
+    description = "The tag of the app image"
+    default = "latest"
+}
+
+variable "app_port" {
+    description = "The port of the app"
+    default = "5000"
+}
